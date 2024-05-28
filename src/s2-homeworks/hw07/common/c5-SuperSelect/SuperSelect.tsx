bad919d87c62ccdbef161ccf84864723d3f0d220
EvgenyStrunevsky <esstrynevskiy@gmail.com>
2024-05-28 14:12:06 +0300

    HW-15 sprint-5-last

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,14 +1,7 @@
-import React, {
-  SelectHTMLAttributes,
-  DetailedHTMLProps,
-  ChangeEvent,
-} from "react";
+import React, { SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent } from "react";
 import s from "./SuperSelect.module.css";
 
-type DefaultSelectPropsType = DetailedHTMLProps<
-  SelectHTMLAttributes<HTMLSelectElement>,
-  HTMLSelectElement
->;
+type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>;
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
   options?: any[];
@@ -22,34 +15,24 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
   onChangeOption,
   ...restProps
 }) => {
-
   const mappedOptions: any[] = options
     ? options.map((o) => (
-        <option
-          id={"hw7-option-" + o.id}
-          className={s.option}
-          key={o.id}
-          value={o.id}
-        >
+        <option id={"hw7-option-" + o.id} className={s.option} key={o.id} value={o.id}>
           {o.value}
         </option>
       ))
     : []; // map options with key
 
   const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-    if(onChangeOption) {
-        onChangeOption(+e.currentTarget.value)
+    if (onChangeOption) {
+      onChangeOption(+e.currentTarget.value);
     }
   };
 
   const finalSelectClassName = s.select + (className ? " " + className : "");
 
   return (
-    <select
-      className={finalSelectClassName}
-      onChange={onChangeCallback}
-      {...restProps}
-    >
+    <select className={finalSelectClassName} onChange={onChangeCallback} {...restProps}>
       {mappedOptions}
     </select>
   );
